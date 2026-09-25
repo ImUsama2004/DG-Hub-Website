34c667836d79d33860ec53b318ba57aa5bc9d3a4
ImUsama2004 <em.usama2004@gmail.com>
2026-09-25 22:56:06 +0500

    Testing SSM Deployment

--- terraform/AWS EC2/github_actions_policy.tf
+++ terraform/AWS EC2/github_actions_policy.tf
@@ -9,14 +9,22 @@ resource "aws_iam_user_policy" "dg_hub_github_actions" {
         Effect = "Allow"
 
         Action = [
-          "ssm:SendCommand",
-          "ssm:GetCommandInvocation"
+          "ssm:SendCommand"
         ]
 
         Resource = [
           "arn:aws:ec2:ap-southeast-2:782893933254:instance/i-0986186c0d9594de1",
           "arn:aws:ssm:ap-southeast-2::document/AWS-RunShellScript"
         ]
+      },
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ssm:GetCommandInvocation"
+        ]
+
+        Resource = "*"
       }
     ]
   })
